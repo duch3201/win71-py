acafc80378340827f9b7fa52cf326f391993049b
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 00:47:44 +0100

    bless this mess
    :)

--- drive0/root/main.py
+++ drive0/root/main.py
@@ -1,44 +1,76 @@
 import sys
+import os
 
-def main(): 
-    command = input("Enter command: ")
-    if command == "quit":
-        FTP.close()
-    if command == "cd":
-        directory = input("Enter directory: ")
-        FTP.cwd(directory)
-        main()
-    if command == "mkdir":
-        directory = input("Enter directory: ")
-        FTP.mkd(directory)
-        main()
-    if command == "rmdir":
-        directory = input("Enter directory: ")
-        FTP.rmd(directory)
-        main()
-    if command == "ls":
-        FTP.retrlines("LIST")
-        main()
-    if command == "rm":
-        file = input("Enter file: ")
-        print("are you sure you wan't to delete the file? (Y/n)")
-        option = input(": ")
-        if option == "Y" or option == "y":
-            FTP.delete(file)
+
+
+def shutdown():
+    print("Shutting down...")
+    sys.exit()
+
+def main(username): 
+    readreqfils()
+
+    print("Hello, " + username + "!")
+    print("What would you like to do?")
+
+    try:
+        command = input("Enter command: ")
+        if command == "quit":
+            shutdown()
+        if command == "cd":
+            directory = input("Enter directory: ")
+            os.cwd(directory)
             main()
-        if option == "N" or option == "n":
+        if command == "mkdir":
+            directory = input("Enter directory: ")
+            os.mkdir(directory)
             main()
-    if command == "help":
-        print("---------------|help|---------------")  
-        print("""
-        cd - change directory
-        mkdir - make directory
-        rmdir - remove directory
-        ls - list directory
-        rm - remove file
-        quit - quit program
-        """)
-        main()
+        if command == "rmdir":
+            directory = input("Enter directory: ")
+            os.rmdir(directory)
+            main()
+        if command == "ls":
+            FTP.retrlines("LIST")
+            main()
+        if command == "rm":
+            file = input("Enter file: ")
+            print("are you sure you wan't to delete the file? (Y/n)")
+            option = input(": ")
+            if option == "Y" or option == "y":
+                os.remove(file)
+                print("% s has been removed successfully" % file)
+                main()
+            if option == "N" or option == "n":
+                print("operation canceled")
+                main()
+        if command == "help":
+            print("---------------|help|---------------")  
+            print("""
+            cd - change directory
+            mkdir - make directory
+            rmdir - remove directory
+            ls - list directory
+            rm - remove file
+            quit - shutdown the system
+            """)
+            main()
+    except KeyboardInterrupt:
+        shutdown()
 
 
-main()
+def readreqfils():
+    print("Reading required files...")
+    os.chdir("tmp")
+    tmpusrfile = ""
+    username = ""
+    username = open( tmpusrfile, "r")
+    print("done!")
+    main(username)
+
+
+
+
+
+
+
+
--- drive0/root/syslogon.py
+++ drive0/root/syslogon.py
@@ -1,15 +1,43 @@
 import sys
 import os
+import main
 
 os.chdir("..")
 os.chdir("home")
 
-print("welcome to win71!", '\n', "please login")
-username = input("username: ")
-password = input("password: ")
+def login():
+    try:
+        print("welcome to win71!", '\n', "please login")
+        username = input("username: ")
+        password = input("password: ")
 
-try:
-    usrfile = open(username + ".user", "rw")
-except FileNotFoundError:
-    print("incorrect username or password")
-    sys.exit()
+        try:
+            usrfile = open(username + ".user", "rw")
+        except FileNotFoundError:
+            print("incorrect username or password")
+            sys.exit()
+
+        with open(usrfile) as file:
+            contents = file.read()
+            search_word = input("Loggedin=TRUE")
+            if search_word in contents:
+                usrfile[1] = "Loggedin=FALSE"
+                with open('example.txt', 'w', encoding='utf-8') as file:
+                    file.writelines(usrfile)
+
+                print("an error accured and you have been logged out")
+                login()
+            else:
+                if usrfile.read() == password:
+                    print("login successful")
+                    os.chdir("..")
+                    os.chdir("tmp")
+                    tmpusrfile = open("rw")
+                    tmpusrfile.write(username)
+                    tmpusrfile.close()
+                    os.chdir("..")
+                    main.main()
+
+
+    except KeyboardInterrupt:
+        print("keybord interupt detected!")
