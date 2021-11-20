caf975149574ecd7cd6a68c79a45bb6642e4401f
BuildTools <56601374+duch3201@users.noreply.github.com>
2021-11-20 01:34:34 +0100

    push

--- drive0/root/main.py
+++ drive0/root/main.py
@@ -1,6 +1,10 @@
 import sys
 import os
+import syslogon
 
+
+username = ""
+setupfinised = True
 
 
 def shutdown():
@@ -8,71 +12,93 @@ def shutdown():
     #need to add a proper way to shutdown the system
     sys.exit()
 
-def main(username): 
-    readreqfils()
+print("Hello, " + username + "!")
+print("What would you like to do?")
 
-    print("Hello, " + username + "!")
-    print("What would you like to do?")
-
-    try:
-        command = input("Enter command: ")
-        if command == "quit":
-            shutdown()
-        if command == "cd":
-            directory = input("Enter directory: ")
-            os.cwd(directory)
-            main()
-        if command == "mkdir":
-            directory = input("Enter directory: ")
-            os.mkdir(directory)
-            main()
-        if command == "rmdir":
-            directory = input("Enter directory: ")
-            os.rmdir(directory)
-            main()
-        if command == "ls": #fix this so it will list files and directories
-            FTP.retrlines("LIST")
-            main()
-        if command == "rm":
-            file = input("Enter file: ") #add a warning for deleting system files
-            print("are you sure you wan't to delete the file? (Y/n)")
-            option = input(": ")
-            if option == "Y" or option == "y":
-                os.remove(file)
-                print("% s has been removed successfully" % file)
-                main()
-            if option == "N" or option == "n":
-                print("operation canceled")
-                main()
-        if command == "help":
-            print("---------------|help|---------------")  
-            print("""
-            cd - change directory
-            mkdir - make directory
-            rmdir - remove directory
-            ls - list directory
-            rm - remove file
-            quit - shutdown the system
-            """)
-            main()
-    except KeyboardInterrupt:
-        shutdown()
-
-
-def readreqfils():
+def main(username, setupfinised): 
+    
     print("Reading required files...")
     os.chdir("..")
     os.chdir("tmp")
-    tmpusrfile = ""
-    username = ""
-    username = open( tmpusrfile, "r")
+    username = open("tmpusrfile.user", "r") 
+    username.read()
+    print(username)
+    os.chdir("..")
+    os.chdir("home")
+    os.chdir(username)   
     print("done!")
-    main(username)
+
+    if setupfinised == False:
+        print("lol")
+
+    else:
+
+        try:
+            command = input("Enter command: ")
+            if command == "quit":
+                shutdown()
+            if command == "cd":
+                directory = input("Enter directory: ")
+                os.chdir(directory)
+                main(username, setupfinised)
+            if command == "mkdir":
+                directory = input("Enter directory: ")
+                os.mkdir(directory, setupfinised)
+                main(username, setupfinised)
+            if command == "rmdir":
+                directory = input("Enter directory: ")
+                os.rmdir(directory)
+                main(username, setupfinised)
+            #if command == "ls": #fix this so it will list files and directories
+             #   FTP.retrlines("LIST")
+              #  main(username, setupfinised)
+            if command == "rm":
+                file = input("Enter file: ") #add a warning for deleting system files
+                print("are you sure you wan't to delete the file? (Y/n)")
+                option = input(": ")
+                if option == "Y" or option == "y":
+                    os.remove(file)
+                    print("% s has been removed successfully" % file)
+                    main(username, setupfinised)
+                if option == "N" or option == "n":
+                    print("operation canceled")
+                    main(username, setupfinised)
+            if command == "help":
+                print("---------------|help|---------------")  
+                print("""
+                cd - change directory
+                mkdir - make directory
+                rmdir - remove directory
+                ls - list directory
+                rm - remove file
+                quit - shutdown the system
+                """)
+                main(username, setupfinised)
+        except KeyboardInterrupt:
+            shutdown()
 
 
-main("username")
 
 
+
+main(username, setupfinised)
+
+
+
+
+
+#def readreqfils(setupfinised):
+ #   setupfinised = "False"
+  #  os.getcwd()
+   # print("Reading required files...")
+    #os.chdir("..")
+    #os.chdir("tmp")
+    #tmpusrfile = ""
+    #username = ""
+    ##username = open( tmpusrfile, "r")
+    #print("done!")
+    #setupfinised = "True"
+    #main(username)
 
 
 
--- drive0/root/syslogon.py
+++ drive0/root/syslogon.py
@@ -1,46 +1,67 @@
 import sys
 import os
-import main
+#import main
 
 os.chdir("..")
-os.chdir("home")
+os.chdir("tmp")
 
 def login():
     try:
+        test = os.getcwd()
+        print(test)
         print("welcome to win71!", '\n', "please login")
         username = input("username: ")
         password = input("password: ")
-
-        try:
-            usrfile = open(username + ".user", "r")
-            usrfile = open(username + ".user", "w")
-        except FileNotFoundError:
-            print("incorrect username or password")
-            sys.exit()
-
-        with open(usrfile) as file:
-            contents = file.read()
-            search_word = input("Loggedin=TRUE") # this 
-            if search_word in contents:
-                usrfile[1] = "Loggedin=FALSE"
-                with open('example.txt', 'w', encoding='utf-8') as file:
-                    file.writelines(usrfile)
-
-                print("an error accured and you have been logged out")
-                login()
-            else:
-                if usrfile.read() == password:
-                    print("login successful")
-                    os.chdir("..")
-                    os.chdir("tmp")
-                    tmpusrfile = open("rw")
-                    tmpusrfile.write(username)
-                    tmpusrfile.close()
-                    os.chdir("..")
-                    main.main()
-
+        tempwrite = open("tmpusrfile", "w")
+        tempwrite.write(username)
+        tempwrite.write(password)
+        tempwrite.close()        
+        os.chdir("..")
+        os.chdir("root")
+        
+        return username
 
     except KeyboardInterrupt:
         print("keybord interupt detected!")
 
 login()
+
+
+
+
+
+
+        #try:
+         #   Wusrfile = open(username + ".user", "w")
+         #   usrfile = open(username + ".user", "r")    
+       # except FileNotFoundError:
+        #    print("incorrect username or password")
+         #   sys.exit()
+
+        #usrfile = open(usrfile, "r")
+        #usrfile.read()
+        #print(usrfile.read())
+        #test = input("press enter to continue")
+
+
+
+        #with open(usrfile, 'r') as file:
+         #   file = file.dump()
+          #  search_word = input("Loggedin=TRUE") # this 
+           # if search_word in usrfile:
+            #    usrfile[1] = "Loggedin=FALSE"
+             #   with open(usrfile, 'w', encoding='utf-8') as file:
+              #      file.writelines(usrfile)
+               #
+                #print("an error accured and you have been logged out")
+                #login()
+            #else:
+             #   if usrfile.read() == password:
+              #      print("login successful")
+               #     os.chdir("..")
+                #    os.chdir("tmp")
+                 #   tmpusrfile = open("rw")
+                  #  tmpusrfile.write(username)
+                   # tmpusrfile.close()
+                    #os.chdir("..")
+                    #os.system('python main.py')
